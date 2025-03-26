a2229fde820af8db0c570979e79c558d68808385
andrecruz0 <andrebrbug@gmail.com>
2025-03-26 16:18:48 -0300

    retirado renderEvents

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,26 +4,11 @@ import { getRepositories } from "./services/repositories.js"
 import {user} from "./objects/user.js"
 import { screen } from "./objects/screen.js"
 import { getEvents } from "./services/events.js"
-
-async function getsexo(){
-    const response = await fetch(`https://api.github.com/users/andrecruz0/repos`)
-
-    
-     await response.json() 
-    console.log(response.json);
-    
-    
-}
-getsexo()
-
-
 getEvents()
 document.querySelector("#btn-search").addEventListener("click", ()=>{
     const userName = document.querySelector("#input-search").value
     if(validateEmptyInput(userName)) return
-    getUserData(userName)
-
-    
+    getUserData(userName) 
 })
 document.querySelector("#input-search").addEventListener("keyup", (e)=>{
     const userName = e.target.value
@@ -41,9 +26,7 @@ function validateEmptyInput(userName){
     }
 }
 async function getUserData(userName){
-
 const userResponse  =   await getUser(userName)
-console.log(userResponse);
 if(userResponse.message === "Not Found"){
     screen.renderNotFound()
     return 
@@ -53,6 +36,5 @@ if(userResponse.message === "Not Found"){
     user.setInfo(userResponse)
     user.setRepositories(repositorieResponse)
     user.setEvents(responseEvents)
-    screen.renderEvents(user)
     screen.renderUser(user)
 }
